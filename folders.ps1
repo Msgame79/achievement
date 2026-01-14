2baee13e681891f8a4600a49762601818ccdf824
Msgames79 <102644909+Msgame79@users.noreply.github.com>
2026-01-15 03:18:36 +0900

    yatto

--- folders.ps1
+++ folders.ps1
@@ -3,14 +3,13 @@ Set-Location $PSScriptRoot
 $a = Invoke-RestMethod "https://raw.githubusercontent.com/Msgame79/AchVideos/refs/heads/main/a.json"
 
 $Levels = $a.psobject.properties.name
-$Achievements = @()
-foreach ($Level in $Levels) {
-    $Achievements += @($a.$Level)
-}
 for ($i = 0; $i -lt $Levels.Length; $i++) {
-    New-Item -ItemType Directory "$($Levels[$i])"
-    foreach ($Achievement in $Achievements[$i]) {
-        New-Item -ItemType Directory "$($Levels[$i])\${Achievement}"
+    while (Test-Path "$($Levels[$i])") {
+        Remove-Item "$($Levels[$i])" -Recurse -Force
+    }
+    New-Item -ItemType Directory "$($Levels[$i])" | Out-Null
+    foreach ($Achievement in $a.($Levels[$i])) {
+        New-Item -ItemType Directory "$($Levels[$i])\${Achievement}" | Out-Null
         Copy-Item -Path ".\template.mlt" -Destination ".\$($Levels[$i])\${Achievement}"
     }
 }
